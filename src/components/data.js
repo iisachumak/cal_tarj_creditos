3efa184feb1f033a08dbd8dc59842ff0b44f9ae8
unknown <iisa@yuhmak.com.ar>
2024-07-02 11:04:38 -0300

    cambios 02072024

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,6 @@
 
 export const data = [
+    /**Bancarazida - Plan Simple */
     {
         id: 1,
         name: 'Bancarazida - Plan Simple',
@@ -10,25 +11,52 @@ export const data = [
             { plazo: 12, financiacion: 1.39 / 12 }
         ],
     },
+    /**AMEX (Santander, Macro, Patagonia, Hsbc, Galicia) */
     {
         id: 2,
         name: 'AMEX (Santander, Macro, Patagonia, Hsbc, Galicia)',
         cuotas: [
-            { plazo: 3, financiacion: 1.13 / 3 },
-            { plazo: 6, financiacion: 1.25 / 6 },
+            { plazo: 3, financiacion: 1.12 / 3 },
+            { plazo: 6, financiacion: 1.21 / 6 },
+            { plazo: 9, financiacion: 1.30 / 9 },
+            { plazo: 12, financiacion: 1.40 / 12 },
         ],
     },
+    /**Argencard, Nativa, CABAL */
     {
         id: 3,
-        name: 'Argencard, Nativa, CABAL, SOL',
+        name: 'Argencard, Nativa, CABAL',
         cuotas: [
             { plazo: 3, financiacion: 1.11 / 3 },
             { plazo: 6, financiacion: 1.20 / 6 },
         ],
     },
+    /**Amex*/
     {
         id: 4,
-        name: 'Amex, Cabal, Dinner, Visa/Master No Bca',
+        name: 'Amex',
+        cuotas: [
+            { plazo: 3, financiacion: 1.31 / 3 },
+            { plazo: 6, financiacion: 1.55 / 6 },
+            { plazo: 12, financiacion: 2.46 / 12 },
+            { plazo: 18, financiacion: 3.04 / 18 },
+        ],
+    },
+    /**Cabal*/
+    {
+        id: 5,
+        name: 'Cabal',
+        cuotas: [
+            { plazo: 3, financiacion: 1.25 / 3 },
+            { plazo: 6, financiacion: 1.45 / 6 },
+            { plazo: 12, financiacion: 2.12 / 12 },
+            { plazo: 18, financiacion: 2.75 / 18 },
+        ],
+    },
+    /**Dinner*/
+    {
+        id: 6,
+        name: 'Dinner',
         cuotas: [
             { plazo: 3, financiacion: 1.35 / 3 },
             { plazo: 6, financiacion: 1.67 / 6 },
@@ -36,32 +64,57 @@ export const data = [
             { plazo: 18, financiacion: 3.12 / 18 },
         ],
     },
+    /**Visa/Master No Bca */
     {
-        id: 5,
+        id: 7,
+        name: 'Visa/Master No Bca',
+        cuotas: [
+            { plazo: 3, financiacion: 1.25 / 3 },
+            { plazo: 6, financiacion: 1.67 / 6 },
+            { plazo: 12, financiacion: 2.02 / 12 },
+            { plazo: 18, financiacion: 2.75 / 18 },
+        ],
+    },
+    /**Naranja */
+    {
+        id: 8,
         name: 'Naranja',
         cuotas: [
             { plazo: 6, financiacion: 1.15 / 6 },
             { plazo: 12, financiacion: 1.39 / 12 },
         ],
     },
+    /**SuCredito */
     {
-        id: 6,
+        id: 9,
         name: 'SuCredito',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
-            { plazo: 3, financiacion: 1.28 / 3 },
-            { plazo: 6, financiacion: 1.78 / 6 },
-            { plazo: 12, financiacion: 2.89 / 12 },
+            { plazo: 3, financiacion: 1.15 / 3 },
+            { plazo: 6, financiacion: 1.70 / 6 },
+            { plazo: 12, financiacion: 2.61 / 12 },
         ],
     },
+    /**Credicash */
     {
-        id: 7,
+        id: 10,
         name: 'Credicash',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
-            { plazo: 3, financiacion: 1.70 / 3 },
-            { plazo: 6, financiacion: 1.46 / 6 },
-            { plazo: 12, financiacion: 2.97 / 12 },
+            { plazo: 3, financiacion: 1.32 / 3 },
+            { plazo: 6, financiacion: 1.41 / 6 },
+            { plazo: 12, financiacion: 3.44 / 12 },
+        ],
+    },
+    /**Sol */
+    {
+        id: 11,
+        name: 'Sol',
+        cuotas: [
+            { plazo: 1, financiacion: 1.03 / 1 },
+            { plazo: 3, financiacion: 1.13 / 3 },
+            { plazo: 6, financiacion: 1.22 / 6 },
+            { plazo: 12, financiacion: 1.94 / 12 },
         ],
     },
 ]
