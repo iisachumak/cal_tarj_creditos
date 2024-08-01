6787b9aed70015de55ebf05bc12bf61fdcc7aeb5
unknown <iisa@yuhmak.com.ar>
2024-08-01 09:26:34 -0300

    cambios 01082024

--- src/components/data.js
+++ src/components/data.js
@@ -34,59 +34,36 @@ export const data = [
     /**Amex*/
     {
         id: 4,
-        name: 'Amex',
+        name: 'Amex - Cualquier Bco',
         cuotas: [
-            { plazo: 3, financiacion: 1.31 / 3 },
-            { plazo: 6, financiacion: 1.55 / 6 },
-            { plazo: 12, financiacion: 2.46 / 12 },
-            { plazo: 18, financiacion: 3.04 / 18 },
-        ],
-    },
-    /**Cabal*/
-    {
-        id: 5,
-        name: 'Cabal',
-        cuotas: [
-            { plazo: 3, financiacion: 1.25 / 3 },
-            { plazo: 6, financiacion: 1.45 / 6 },
-            { plazo: 12, financiacion: 2.12 / 12 },
-            { plazo: 18, financiacion: 2.75 / 18 },
-        ],
-    },
-    /**Dinner*/
-    {
-        id: 6,
-        name: 'Dinner',
-        cuotas: [
-            { plazo: 3, financiacion: 1.35 / 3 },
-            { plazo: 6, financiacion: 1.67 / 6 },
-            { plazo: 12, financiacion: 2.64 / 12 },
-            { plazo: 18, financiacion: 3.12 / 18 },
+            { plazo: 15, financiacion: 3.20 / 15 },
+            { plazo: 18, financiacion: 4.19 / 18 },
         ],
     },
     /**Visa/Master No Bca */
     {
-        id: 7,
+        id: 5,
         name: 'Visa/Master No Bca',
         cuotas: [
-            { plazo: 3, financiacion: 1.25 / 3 },
-            { plazo: 6, financiacion: 1.67 / 6 },
-            { plazo: 12, financiacion: 2.02 / 12 },
-            { plazo: 18, financiacion: 2.75 / 18 },
+            { plazo: 3, financiacion: 1.23 / 3 },
+            { plazo: 6, financiacion: 1.41 / 6 },
+            { plazo: 12, financiacion: 1.89 / 12 },
+            { plazo: 18, financiacion: 2.50 / 18 },
         ],
     },
     /**Naranja */
     {
-        id: 8,
+        id: 6,
         name: 'Naranja',
         cuotas: [
             { plazo: 6, financiacion: 1.15 / 6 },
+            { plazo: 9, financiacion: 1.26 / 9 },
             { plazo: 12, financiacion: 1.39 / 12 },
         ],
     },
     /**SuCredito */
     {
-        id: 9,
+        id: 7,
         name: 'SuCredito',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
@@ -97,18 +74,18 @@ export const data = [
     },
     /**Credicash */
     {
-        id: 10,
+        id: 8,
         name: 'Credicash',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
             { plazo: 3, financiacion: 1.32 / 3 },
             { plazo: 6, financiacion: 1.41 / 6 },
-            { plazo: 12, financiacion: 3.44 / 12 },
+            { plazo: 12, financiacion: 4.44 / 12 },
         ],
     },
     /**Sol */
     {
-        id: 11,
+        id: 9,
         name: 'Sol',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
