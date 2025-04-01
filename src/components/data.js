cb515396196d3e43e133e4d4e7b95fa1752ea2fd
unknown <iisa@yuhmak.com.ar>
2025-04-01 11:29:48 -0300

    cambios 01042025 01

--- src/components/data.js
+++ src/components/data.js
@@ -5,8 +5,8 @@ export const data = [
         id: 1,
         name: 'Bancarazida - Plan Simple',
         cuotas: [
-            { plazo: 3, financiacion: 1.11 / 3 },
-            { plazo: 6, financiacion: 1.19 / 6 },
+            { plazo: 3, financiacion: 1.10 / 3 },
+            { plazo: 6, financiacion: 1.18 / 6 },
             /** { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.34 / 12 }*/
             
@@ -17,7 +17,7 @@ export const data = [
         id: 2,
         name: 'AMEX (Santander, Macro, Patagonia, Hsbc, Galicia)',
         cuotas: [
-            { plazo: 3, financiacion: 1.11 / 3 },
+            { plazo: 3, financiacion: 1.12 / 3 },
             { plazo: 6, financiacion: 1.20 / 6 },
             /** { plazo: 9, financiacion: 1.26 / 9 },
             { plazo: 12, financiacion: 1.35 / 12 },*/
@@ -28,8 +28,8 @@ export const data = [
         id: 3,
         name: 'Nativa, CABAL',
         cuotas: [
-            { plazo: 3, financiacion: 1.11 / 3 },
-            { plazo: 6, financiacion: 1.19 / 6 },
+            { plazo: 3, financiacion: 1.10 / 3 },
+            { plazo: 6, financiacion: 1.18 / 6 },
             /** { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.34 / 12 }*/
         ],
@@ -39,10 +39,10 @@ export const data = [
         id: 4,
         name: 'Bcada - Otros Bancos',
         cuotas:[
-            { plazo: 9, financiacion: 1.32 / 9 },
-            { plazo: 12, financiacion: 1.43 / 12 },
-            { plazo: 15, financiacion: 1.55 / 15 },
-            { plazo: 18, financiacion: 1.67 / 18 }
+            { plazo: 9, financiacion: 1.25 / 9 },
+            { plazo: 12, financiacion: 1.33 / 12 },
+            { plazo: 15, financiacion: 1.52 / 15 },
+            { plazo: 18, financiacion: 1.51 / 18 }
         ]
     },
     /**VISA- MASTER-BCO ,FRANCES- SANTANDER-GALICIA-NACION-MACRO */
@@ -50,10 +50,10 @@ export const data = [
         id: 5,
         name: 'Bcada - Frances, Santander, Galicia, Nacion, Macro',
         cuotas: [
-            { plazo: 9, financiacion: 1.64 / 9 },
-            { plazo: 12, financiacion: 1.89 / 12 },
-            { plazo: 15, financiacion: 2.17 / 15 },
-            { plazo: 18, financiacion: 2.50 / 18 }
+            { plazo: 9, financiacion: 1.59 / 9 },
+            { plazo: 12, financiacion: 1.81 / 12 },
+            { plazo: 15, financiacion: 2.07 / 15 },
+            { plazo: 18, financiacion: 2.35 / 18 }
         ]
     },
     /**Visa/Master No Bca */
@@ -61,10 +61,10 @@ export const data = [
         id: 6,
         name: 'Visa/Master No Bca',
         cuotas: [
-            { plazo: 3, financiacion: 1.23 / 3 },
-            { plazo: 6, financiacion: 1.41 / 6 },
-            { plazo: 12, financiacion: 1.89 / 12 },
-            { plazo: 18, financiacion: 2.50 / 18 },
+            { plazo: 3, financiacion: 1.21 / 3 },
+            { plazo: 6, financiacion: 1.38 / 6 },
+            { plazo: 12, financiacion: 1.81 / 12 },
+            { plazo: 18, financiacion: 2.35 / 18 },
         ],
     },
     /**CABAL NO SIMPLE OTROS BANCOS  */
@@ -83,9 +83,9 @@ export const data = [
         id: 8,
         name: 'AMEX NO SIMPLE OTROS BANCOS',
         cuotas: [
-            { plazo: 3, financiacion: 1.29 / 3 },
-            { plazo: 6, financiacion: 1.57 / 6 },
-            { plazo: 12, financiacion: 2.50 / 12 },
+            { plazo: 3, financiacion: 1.22 / 3 },
+            { plazo: 6, financiacion: 1.41 / 6 },
+            { plazo: 12, financiacion: 1.97 / 12 },
         ],
     },
     /**Naranja */
@@ -94,8 +94,7 @@ export const data = [
         name: 'Naranja',
         cuotas: [
             { plazo: 6, financiacion: 1.15 / 6 },
-            { plazo: 9, financiacion: 1.26 / 9 },
-            { plazo: 12, financiacion: 1.39 / 12 },
+            { plazo: 12, financiacion: 1.37 / 12 },
         ],
     },
     /**SuCredito 
