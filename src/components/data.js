8df33421e87ee95c3b8d9d2f69160be00437580c
ismael isa <iisa@yuhmak.com.ar>
2025-06-05 09:38:04 -0300

    cambios 05062025 01

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,6 @@
 
 export const data = [
+
     /**Bancarazida - Plan Simple */
     {
         id: 1,
@@ -12,6 +13,7 @@ export const data = [
             
         ],
     },
+
     /**AMEX (Santander, Macro, Patagonia, Hsbc, Galicia) */
     {
         id: 2,
@@ -23,6 +25,7 @@ export const data = [
             { plazo: 12, financiacion: 1.35 / 12 },*/
         ],
     },
+
     /**Argencard, Nativa, CABAL */
     {
         id: 3,
@@ -34,53 +37,52 @@ export const data = [
             { plazo: 12, financiacion: 1.34 / 12 }*/
         ],
     },
-    /**VISA / MASTER/DINNER/ CABAL/ NATIVA bancarias  */
+    
+    /** VISA- MASTERCARD TARJETAS BANCARIAS TODOS LOS BANCOS */
     {
         id: 4,
-        name: 'Bcada - Otros Bancos',
+        name: 'VM Bcas todos Bcos.',
         cuotas:[
-            { plazo: 9, financiacion: 1.30 / 9 },
-            { plazo: 12, financiacion: 1.40 / 12 },
-            { plazo: 15, financiacion: 1.51 / 15 },
-            { plazo: 18, financiacion: 1.63 / 18 }
+            { plazo: 9, financiacion: 1.26 / 9 },
+            { plazo: 12, financiacion: 1.34 / 12 },
         ]
     },
-    /**VISA- MASTER-BCO ,FRANCES- SANTANDER-GALICIA-NACION-MACRO */
+
+    /**VISA / MASTER/DINNER/ CABAL/  BANCOS DETALLADOS */
     {
         id: 5,
-        name: 'Bcada - Frances, Santander, Galicia, Nacion, Macro',
+        name: 'VMSC Bcos. Seleccionados',
         cuotas: [
-            { plazo: 9, financiacion: 1.59 / 9 },
-            { plazo: 12, financiacion: 1.81 / 12 },
-            { plazo: 15, financiacion: 2.07 / 15 },
-            { plazo: 18, financiacion: 2.35 / 18 }
+            { plazo: 15, financiacion: 1.42 / 15 },
+            { plazo: 18, financiacion: 1.51 / 18 }
         ]
     },
-    /**Visa/Master No Bca */
+
+    /**NO BANCARIAS :VISA / MASTER . (NARANJA ,CENCOSUD,EFECTIVO ,UALA,BRUBANK,MERC.LIBRE , ETC) */
     {
         id: 6,
-        name: 'Visa/Master No Bca',
+        name: 'No Bancarias VM',
         cuotas: [
-            { plazo: 3, financiacion: 1.21 / 3 },
-            { plazo: 6, financiacion: 1.38 / 6 },
-            { plazo: 12, financiacion: 1.81 / 12 },
-            { plazo: 18, financiacion: 2.35 / 18 },
+            { plazo: 3, financiacion: 1.17 / 3 },
+            { plazo: 6, financiacion: 1.33 / 6 },
+            { plazo: 12, financiacion: 1.74 / 12 },
+            { plazo: 18, financiacion: 2.25 / 18 },
         ],
     },
-    /**CABAL NO SIMPLE OTROS BANCOS  */
+
+    /**CABAL NO SIMPLE OTROS BANCOS */
     {
         id: 7,
         name: 'CABAL NO SIMPLE OTROS BANCOS',
         cuotas: [
-            /**
             { plazo: 3, financiacion: 1.25 / 3 },
             { plazo: 6, financiacion: 1.45 / 6 },
-              */
             { plazo: 12, financiacion: 2.02 / 12 },
             { plazo: 18, financiacion: 2.75 / 18 }
         ],
     },
-    /**AMEX NO SIMPLE OTROS BANCOS  */
+
+    /**AMEX NO SIMPLE OTROS BANCOS */
     {
         id: 8,
         name: 'AMEX NO SIMPLE OTROS BANCOS',
@@ -91,26 +93,29 @@ export const data = [
             { plazo: 18, financiacion: 2.81 / 18 },
         ],
     },
+
     /**Naranja */
     {
         id: 9,
         name: 'Naranja',
         cuotas: [
-            { plazo: 6, financiacion: 1.16 / 6 },
-            { plazo: 12, financiacion: 1.37 / 12 },
+            { plazo: 6, financiacion: 1.17 / 6 },
+            { plazo: 9, financiacion: 1.26 / 9 },
+            { plazo: 12, financiacion: 1.39 / 12 },
             { plazo: 14, financiacion: 1.51 / 14 },
         ],
     },
+
     /**SuCredito */
     {
         id: 10,
         name: 'SuCredito',
         cuotas: [
-            /*{ plazo: 1, financiacion: 1.03 / 1 },*/
-            { plazo: 3, financiacion: 1.10 / 3 },
-            { plazo: 6, financiacion: 1.21 / 6 },
-            { plazo: 9, financiacion: 1.32 / 9 },
-            { plazo: 12, financiacion: 1.46 / 12 },
+            { plazo: 1, financiacion: 1.03 / 1 },
+            { plazo: 3, financiacion: 1.06 / 3 },
+            { plazo: 6, financiacion: 1.16 / 6 },
+            { plazo: 9, financiacion: 1.26 / 9 },
+            { plazo: 12, financiacion: 1.39 / 12 },
         ],
     },
     
@@ -119,32 +124,25 @@ export const data = [
         id: 11,
         name: 'Credicash',
         cuotas: [
-            /*{ plazo: 1, financiacion: 1.03 / 1 },*/
+            { plazo: 1, financiacion: 1.03 / 1 },
             { plazo: 3, financiacion: 1.10 / 3 },
             { plazo: 6, financiacion: 1.30 / 6 },
             { plazo: 9, financiacion: 1.49 / 9 },
             { plazo: 12, financiacion: 1.90 / 12 },
         ],
     },
+
     /**Sol */
     {
         id: 12,
         name: 'Sol',
         cuotas: [
-            /*{ plazo: 1, financiacion: 1.03 / 1 },*/
+            { plazo: 1, financiacion: 1.03 / 1 },
             { plazo: 3, financiacion: 1.11 / 3 },
             { plazo: 6, financiacion: 1.18 / 6 },
             { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.33 / 12 },
         ],
     },
-    /**MACRO */
-    {
-        id: 13,
-        name: 'Solo Macro Visa/Master',
-        cuotas: [
-            { plazo: 9, financiacion: 1.26 / 9 },
-            { plazo: 12, financiacion: 1.36 / 12 },
-        ],
-    },
+    
 ]
