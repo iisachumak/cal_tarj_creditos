bf5442d5fda905117d40d53f6ce400fb6c8d8f92
Ismael Isa <ismael.isa02@gmail.com>
2025-08-05 12:43:41 -0300

    cambios 05082025 01

--- src/components/data.js
+++ src/components/data.js
@@ -4,10 +4,10 @@ export const data = [
     /**Bancarazida - Plan Simple */
     {
         id: 1,
-        name: 'Bancarazida - Plan Simple',
+        name: 'VISA Y MASTERCARD  NATIVA CABAL',
         cuotas: [
-            { plazo: 3, financiacion: 1.10 / 3 },
-            { plazo: 6, financiacion: 1.18 / 6 },
+            { plazo: 3, financiacion: 1.11 / 3 },
+            { plazo: 6, financiacion: 1.19 / 6 },
             /** { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.34 / 12 }*/
             
@@ -17,7 +17,7 @@ export const data = [
     /**AMEX (Santander, Macro, Patagonia, Hsbc, Galicia) */
     {
         id: 2,
-        name: 'AMEX (Santander, Macro, Patagonia, Hsbc, Galicia)',
+        name: 'AMEX ( Bco Santander ,Macro ,Patagonia ,Hsbc,Galicia )',
         cuotas: [
             { plazo: 3, financiacion: 1.12 / 3 },
             { plazo: 6, financiacion: 1.20 / 6 },
@@ -29,10 +29,10 @@ export const data = [
     /**Argencard, Nativa, CABAL */
     {
         id: 3,
-        name: 'Nativa, CABAL',
+        name: 'VISA- MASTERCARD: Santander, Macro,Galicia y Frances',
         cuotas: [
-            { plazo: 3, financiacion: 1.10 / 3 },
-            { plazo: 6, financiacion: 1.18 / 6 },
+            { plazo: 9, financiacion: 1.38 / 9 },
+            { plazo: 12, financiacion: 1.52 / 12 },
             /** { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.34 / 12 }*/
         ],
@@ -41,18 +41,20 @@ export const data = [
     /** VISA- MASTERCARD TARJETAS BANCARIAS TODOS LOS BANCOS */
     {
         id: 4,
-        name: 'VM Bcas todos Bcos.',
+        name: 'BCO SANTANDER GETNET',
         cuotas:[
-            { plazo: 9, financiacion: 1.26 / 9 },
-            { plazo: 12, financiacion: 1.34 / 12 },
+            { plazo: 9, financiacion: 1.28 / 9 },
+            { plazo: 12, financiacion: 1.36 / 12 },
         ]
     },
 
     /**VISA / MASTER/DINNER/ CABAL/  BANCOS DETALLADOS */
     {
         id: 5,
-        name: 'VMSC Bcos. Seleccionados',
+        name: 'VISA / MASTER/DINNER/ CABAL/ ',
         cuotas: [
+            { plazo: 9, financiacion: 1.25 / 9 },
+            { plazo: 12, financiacion: 1.33 / 12 },
             { plazo: 15, financiacion: 1.42 / 15 },
             { plazo: 18, financiacion: 1.51 / 18 }
         ]
@@ -61,7 +63,7 @@ export const data = [
     /**NO BANCARIAS :VISA / MASTER . (NARANJA ,CENCOSUD,EFECTIVO ,UALA,BRUBANK,MERC.LIBRE , ETC) */
     {
         id: 6,
-        name: 'No Bancarias VM',
+        name: 'NO BANCARIAS :VISA / MASTER',
         cuotas: [
             { plazo: 3, financiacion: 1.17 / 3 },
             { plazo: 6, financiacion: 1.33 / 6 },
@@ -75,10 +77,9 @@ export const data = [
         id: 7,
         name: 'CABAL NO SIMPLE OTROS BANCOS',
         cuotas: [
-            { plazo: 3, financiacion: 1.25 / 3 },
-            { plazo: 6, financiacion: 1.45 / 6 },
-            { plazo: 12, financiacion: 2.02 / 12 },
-            { plazo: 18, financiacion: 2.75 / 18 }
+            { plazo: 3, financiacion: 1.11 / 3 },
+            { plazo: 6, financiacion: 1.19 / 6 },
+            { plazo: 12, financiacion: 1.82 / 12 }
         ],
     },
 
@@ -87,10 +88,10 @@ export const data = [
         id: 8,
         name: 'AMEX NO SIMPLE OTROS BANCOS',
         cuotas: [
-            { plazo: 3, financiacion: 1.22 / 3 },
-            { plazo: 6, financiacion: 1.41 / 6 },
-            { plazo: 12, financiacion: 1.97 / 12 },
-            { plazo: 18, financiacion: 2.81 / 18 },
+            { plazo: 3, financiacion: 1.21 / 3 },
+            { plazo: 6, financiacion: 1.40 / 6 },
+            { plazo: 12, financiacion: 1.93 / 12 },
+            { plazo: 18, financiacion: 2.75 / 18 },
         ],
     },
 
@@ -138,10 +139,11 @@ export const data = [
         name: 'Sol',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
-            { plazo: 3, financiacion: 1.11 / 3 },
+            { plazo: 3, financiacion: 1.10 / 3 },
             { plazo: 6, financiacion: 1.18 / 6 },
             { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.33 / 12 },
+            { plazo: 14, financiacion: 1.34 / 14 }
         ],
     },
     
