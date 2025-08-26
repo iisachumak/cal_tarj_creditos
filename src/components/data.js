ab14d2c93ded50e9434ce8dc01478e97ba7e0c8c
Ismael Isa <ismael.isa02@gmail.com>
2025-08-26 09:35:12 -0300

    cambios 26082025

--- src/components/data.js
+++ src/components/data.js
@@ -6,8 +6,8 @@ export const data = [
         id: 1,
         name: 'VISA Y MASTERCARD  NATIVA CABAL',
         cuotas: [
-            { plazo: 3, financiacion: 1.11 / 3 },
-            { plazo: 6, financiacion: 1.19 / 6 },
+            { plazo: 3, financiacion: 1.14 / 3 },
+            { plazo: 6, financiacion: 1.25 / 6 },
             /** { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.34 / 12 }*/
             
@@ -19,8 +19,8 @@ export const data = [
         id: 2,
         name: 'AMEX ( Bco Santander ,Macro ,Patagonia ,Hsbc,Galicia )',
         cuotas: [
-            { plazo: 3, financiacion: 1.12 / 3 },
-            { plazo: 6, financiacion: 1.20 / 6 },
+            { plazo: 3, financiacion: 1.15 / 3 },
+            { plazo: 6, financiacion: 1.27 / 6 },
             /** { plazo: 9, financiacion: 1.26 / 9 },
             { plazo: 12, financiacion: 1.35 / 12 },*/
         ],
@@ -31,10 +31,8 @@ export const data = [
         id: 3,
         name: 'VISA- MASTERCARD: Santander, Macro,Galicia y Frances',
         cuotas: [
-            { plazo: 9, financiacion: 1.38 / 9 },
-            { plazo: 12, financiacion: 1.52 / 12 },
-            /** { plazo: 9, financiacion: 1.25 / 9 },
-            { plazo: 12, financiacion: 1.34 / 12 }*/
+            { plazo: 9, financiacion: 1.57 / 9 },
+            { plazo: 12, financiacion: 1.80 / 12 },
         ],
     },
     
@@ -43,6 +41,7 @@ export const data = [
         id: 4,
         name: 'BCO SANTANDER GETNET',
         cuotas:[
+            { plazo: 6, financiacion: 1.20 / 9 },
             { plazo: 9, financiacion: 1.28 / 9 },
             { plazo: 12, financiacion: 1.36 / 12 },
         ]
@@ -51,7 +50,7 @@ export const data = [
     /**VISA / MASTER/DINNER/ CABAL/  BANCOS DETALLADOS */
     {
         id: 5,
-        name: 'VISA / MASTER/DINNER/ CABAL/ ',
+        name: 'VISA / MASTER',
         cuotas: [
             { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.33 / 12 },
@@ -65,33 +64,31 @@ export const data = [
         id: 6,
         name: 'NO BANCARIAS :VISA / MASTER',
         cuotas: [
-            { plazo: 3, financiacion: 1.17 / 3 },
-            { plazo: 6, financiacion: 1.33 / 6 },
-            { plazo: 12, financiacion: 1.74 / 12 },
-            { plazo: 18, financiacion: 2.25 / 18 },
+            { plazo: 3, financiacion: 1.34 / 3 },
+            { plazo: 6, financiacion: 1.63 / 6 },
+            { plazo: 12, financiacion: 2.48 / 12 }
         ],
     },
 
     /**CABAL NO SIMPLE OTROS BANCOS */
     {
         id: 7,
-        name: 'CABAL NO SIMPLE OTROS BANCOS',
+        name: 'CABAL NO SIMPLE',
         cuotas: [
-            { plazo: 3, financiacion: 1.11 / 3 },
-            { plazo: 6, financiacion: 1.19 / 6 },
-            { plazo: 12, financiacion: 1.82 / 12 }
+            { plazo: 3, financiacion: 1.34 / 3 },
+            { plazo: 6, financiacion: 1.63 / 6 },
+            { plazo: 12, financiacion: 2.48 / 12 }
         ],
     },
 
     /**AMEX NO SIMPLE OTROS BANCOS */
     {
         id: 8,
-        name: 'AMEX NO SIMPLE OTROS BANCOS',
+        name: 'AMEX NO SIMPLE',
         cuotas: [
-            { plazo: 3, financiacion: 1.21 / 3 },
-            { plazo: 6, financiacion: 1.40 / 6 },
-            { plazo: 12, financiacion: 1.93 / 12 },
-            { plazo: 18, financiacion: 2.75 / 18 },
+            { plazo: 3, financiacion: 1.34 / 3 },
+            { plazo: 6, financiacion: 1.63 / 6 },
+            { plazo: 12, financiacion: 2.48 / 12 }
         ],
     },
 
@@ -100,10 +97,10 @@ export const data = [
         id: 9,
         name: 'Naranja',
         cuotas: [
-            { plazo: 6, financiacion: 1.17 / 6 },
-            { plazo: 9, financiacion: 1.26 / 9 },
-            { plazo: 12, financiacion: 1.39 / 12 },
-            { plazo: 14, financiacion: 1.51 / 14 },
+            { plazo: 6, financiacion: 1.14 / 6 },
+            { plazo: 9, financiacion: 1.29 / 9 },
+            { plazo: 12, financiacion: 1.35 / 12 },
+            { plazo: 14, financiacion: 1.63 / 14 },
         ],
     },
 
@@ -139,8 +136,8 @@ export const data = [
         name: 'Sol',
         cuotas: [
             { plazo: 1, financiacion: 1.03 / 1 },
-            { plazo: 3, financiacion: 1.10 / 3 },
-            { plazo: 6, financiacion: 1.18 / 6 },
+            { plazo: 3, financiacion: 1.13 / 3 },
+            { plazo: 6, financiacion: 1.22 / 6 },
             { plazo: 9, financiacion: 1.25 / 9 },
             { plazo: 12, financiacion: 1.33 / 12 },
             { plazo: 14, financiacion: 1.34 / 14 }
